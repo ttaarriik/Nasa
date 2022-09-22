0a600425bcbd5ecadd64ea7a500105ca2df4927b
Abdul Khaliq <ttaarriik@mail.ru>
2022-09-22 00:26:36 -0700

    Database added to CI

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const app = require('../../app');
 const {mongoConnect, mongoDisconnect} = require('../../services/mongo');
+const {loadPlanetsData} = require('../../models/planets.model');
 describe('Test GET requests', () => {
     beforeAll( async () => {
      await mongoConnect();
+     await loadPlanetsData();
     })
 
     afterAll( async () => {
